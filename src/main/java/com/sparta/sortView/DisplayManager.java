ef25e87d067848c72422504150f04a7ad3a77776
ykolarov <kolarov01@protonmail.me>
2022-07-06 23:08:12 +0100

    Implement a try catch block for user input wrong format

--- src/main/java/com/sparta/sortView/DisplayManager.java
+++ src/main/java/com/sparta/sortView/DisplayManager.java
@@ -1,10 +1,11 @@
 package com.sparta.sortView;
-
-
 import com.sparta.sortController.SortManager;
 import com.sparta.sortModel.AbstractFactory;
 import com.sparta.sortModel.AbstractSorter;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DisplayManager implements View {
@@ -22,7 +23,7 @@ public class DisplayManager implements View {
         while(true) {
             logger.info("Asking user to choose algorithm option");
             System.out.println("\n" + factory.userInstructions() + " 9: Exit the program");
-            int algoChoice = scanner.nextInt();
+            int algoChoice = getIntFromUser(scanner, 0, "algorithm option");
             if(algoChoice==9) {
                 logger.info("User chose to exit the application");
                 break;
@@ -33,11 +34,11 @@ public class DisplayManager implements View {
                 continue;
             }
             logger.info("User chose option of " + sorter.getDescription());
-            logger.info("Asking user to choose length for random array");
-            int lengthChoice = getLengthChoiceFromUser(scanner);
+            System.out.println("Please enter a number for the length of the array");
+            int lengthChoice = getIntFromUser(scanner, 1, "length of array");
             if(lengthChoice>=1000)
                 logger.warn("User inputted a big number for length: " + lengthChoice);
-            else logger.info("Length input:" + lengthChoice);
+            else logger.info("Length of the array has been chosen successfully:" + lengthChoice);
 
             displayAlgorithmInAction(sorter, sorter.generateRandomArray(lengthChoice));
             logger.info("App successfully presented output using " + sorter.getDescription());
@@ -56,17 +57,26 @@ public class DisplayManager implements View {
         printArrayElements(sortedArr);
     }
 
-    private int getLengthChoiceFromUser(Scanner scanner) {
-        System.out.println("Please choose the array length:");
-        int lengthChoice;
+    private int getIntFromUser(Scanner scanner, int minAcceptable, String reason) {
+        int userInput = Integer.MIN_VALUE; // smaller than minimum acceptable value
         do{
-            lengthChoice = scanner.nextInt();
-            if(lengthChoice < 1) System.out.println("Please enter a positive number for length");
-        } while(lengthChoice < 1);
-        return lengthChoice;
+            try{
+                logger.info("Asking user to input a new integer for " + reason);
+                userInput = Integer.parseInt(scanner.next());;
+            } catch(NumberFormatException e) {
+                logger.error("EXCEPTION CAUGHT: User entered bad input for length : " + e.getMessage());
+                System.out.println("Entered number is invalid, please try again");
+                continue;
+            }
+            if(userInput < minAcceptable) {
+                logger.warn("User chose invalid int as input");
+                System.out.println("Entered number is invalid, please try again");
+            }
+        } while(userInput < minAcceptable);
+        return userInput;
     }
 
-    private static void printArrayElements(int[] array) {
+    private void printArrayElements(int[] array) {
         for (int i: array) {
             System.out.print(i + " ");
         }
